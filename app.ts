4272869171c2f611eb8c6fdd2a7de1451d9157bb
havuong <havuong138@gmail.com>
2019-05-02 11:22:06 +0700

    [Function] Rest & Default Parameters

--- app.ts
+++ app.ts
@@ -22,3 +22,28 @@ for (var index = 0; index < studentsList.length; index++) {
     var element = studentsList[index];
     console.log("Age:" + element.Age + " with Name:" + element.Name + " has Phone:" +element.Phone + " knows language:"+ element.Language );
 }
+
+//Function declaration
+function GetStudentsList(students: any[]){
+    students.forEach(element => {
+            console.log("Age:" + element.Age + " with Name:" + element.Name + " has Phone:" +element.Phone + " knows language:"+ element.Language );
+    });
+}
+
+GetStudentsList(studentsList);
+
+//Rest Parameters
+function GetNumbers(...nums: number[]){
+    nums.forEach(element => {
+        console.log("Number: " + element);
+    });
+}
+
+GetNumbers(1,2,3,4,5,6)
+
+//Default Parameters
+function GetInfo(info:string = "Happy"){
+    console.log(info);
+}
+
+GetInfo("Very happy");
